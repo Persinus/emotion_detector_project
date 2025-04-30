062f7eaa624c0d975a00a352450f802371a760f9
Persinus <nguyenmanh2004devgame@gmail.com>
2025-04-30 21:09:33 +0700

    update Back-End fix Deploy

--- Deep-Learning/app.py
+++ Deep-Learning/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/predict": {"origins": ["http://localhost:5173", "https://emotion-detector-project-chaos.onrender.com"]}})
 CORS(app, resources={r"/predict_frame": {"origins": ["http://localhost:5173", "https://emotion-detector-project-chaos.onrender.com"]}})
 CORS(app, resources={r"/status": {"origins": ["http://localhost:5173", "https://emotion-detector-project-chaos.onrender.com"]}})
-
+CORS(app)
 # Load the emotion detection model
 model = load_model('emotion_detection_model.h5')
 
